616af6ec3c2b3bfa0eb75e20c66c8d7900ec1f50
Christopher Hargarten <cphargarten@gmail.com>
2015-12-03 22:05:59 -0600

    don't need access to the file system - remove fs and respective usages.

--- test/parsing.js
+++ test/parsing.js
@@ -19,7 +19,6 @@ describe('Banking', function(){
         url: 'https://www.oasis.cfree.com/3001.ofxgp'
       });
 
-
       //If second param is omitted JSON will be returned by default
       banking.getStatement({start:20131101, end:20131120}, function (err, res) {
         if(err) done(res)
@@ -36,17 +35,6 @@ describe('Banking', function(){
       done();
     });
   })
-
-  describe('.parseFile', function(){
-    it('should read the provided file and return JSON', function(done){
-
-      Banking.parseFile(__dirname +'/fixtures/sample.ofx', function (res) {
-        res.body.should.be.an.instanceof(Object);
-        res.body.should.have.property('OFX');
-        done();
-      });
-    });
-  });
 
   describe('.parse', function(){
     it('should read the provided string and return JSON', function(done){
